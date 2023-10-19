b61c774e86c2d8587c4a61f130a97fab31a57808
Diana rodríguez <128723615+DianaADSO@users.noreply.github.com>
2023-10-19 07:57:52 -0500

    versión 5

--- funciones.php
+++ funciones.php
@@ -2,10 +2,20 @@
 
 //Función para consultar
 function consulta(){
-    $salida = " "; // Inicializa la variable
+    $salida = 0; // Inicializa la variable
 
     //Conexión con la base de datos
-    $conexion =  mysqli_connect('localhost', 'root', 'root', 'db_proyecto_ddm');
+    $conexion = mysqli_connect('localhost', 'root', 'root', 'db_proyecto_ddm');
+    $sql      = "SELECT 2 + 1 "; //Sql para sumar
+    $sql     .= " as 'Suma'"; //sql para poner el alias al anterior sql
+    $resultado = $conexion->query($sql); //Resultado de ejecutar el query
+
+    //Recorre el recordset
+    while($fila = mysqli_fetch_assoc($resultado)){
+
+        //Acumula
+        $salida += $fila['Suma']; 
+    }
 
 
 
